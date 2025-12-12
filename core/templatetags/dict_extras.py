8824aec780be26fa223078d074af896d53b9a1a4
Aguaio <8towea@gmail.com>
2025-12-12 10:11:57 +0100

    Add files via upload

--- core/templatetags/dict_extras.py
+++ core/templatetags/dict_extras.py
@@ -1,7 +1,28 @@
 
 from django import template
+from decimal import Decimal, InvalidOperation
+
 register = template.Library()
+
 
 @register.filter
 def get_item(d, key):
     return d.get(key, [])
+
+
+@register.filter
+def precio_clp(value):
+    """
+    Formatea un valor numerico como CLP: $8.000 (sin decimales, miles con punto).
+    """
+    if value in (None, ''):
+        return '$0'
+    try:
+        dec = Decimal(str(value))
+    except (InvalidOperation, TypeError, ValueError):
+        return '$0'
+
+    dec = dec.quantize(Decimal('1'))
+    num = int(dec)
+    formatted = f'{num:,}'.replace(',', '.')
+    return f'${formatted}'
